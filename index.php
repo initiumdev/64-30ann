87744802a789469ab3a4ce637197f8681fefd1ea
joetsuihk <joe@documentonready.com>
2019-05-29 16:42:10 +0800

    up

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ if($_SERVER['HTTP_HOST'] == 'localhost') {
     $base_url = $scheme.$_SERVER['HTTP_HOST'].'/64-30/';
     $js_base_url = $base_url;
 } elseif(strpos($_SERVER['HTTP_HOST'], 'dev') !== FALSE) {
-    $base_url = '//project/20190529-project-6430/';
+    $base_url = '/project/20190529-project-6430/';
     $js_base_url = $base_url;
 } else {
     $base_url = 'https://theinitium.com/project/20190529-project-6430/';
